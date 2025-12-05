5f1c8c29ce1759361d15bbf47556359fa7b2ed39
Lalo12-max <144458851+Lalo12-max@users.noreply.github.com>
2025-12-05 17:49:37 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
-const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
+
+const PORT = process.env.PORT || 3000;
+const ENVIRONMENT_NAME = process.env.ENVIRONMENT_NAME || 'development';
 const VERSION = process.env.VERSION || '1.0.0';
 
-app.listen(port, () => {
-    console.log(`✅ Servidor producción iniciado en puerto ${port}, ambiente: ${ENVIRONMENT}, versión: ${VERSION}`);
+app.listen(PORT, () => {
+    console.log(`✅ Servidor producción iniciado en puerto ${PORT}, ambiente: ${ENVIRONMENT_NAME}, versión: ${VERSION}`);
 });
